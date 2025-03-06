4938a04416243ed9d76fdd5f55ff1766686b3869
sstvskids <staavstudios@gmail.com>
2025-03-06 19:45:51 +1100

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -10,7 +10,7 @@ cfgkeys.new = function()
     local identifier = tostring(math.random(1,9999999))
     local key = sha.sha256(identifier)
     local fullkey = 'CFGKEY-'..key
-    cfgkeys.clientkeys[user] = 'CFGKEY-'..fullkey
+    cfgkeys.clientkeys[user] = fullkey
     writefile('cfgkey.lua', cfgkeys.clientkeys[user])
     return fullkey
 end
