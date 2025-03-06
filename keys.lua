cdfeae8e015c5d5c1bc60ffd3ec85bec01c989c1
sstvskids <staavstudios@gmail.com>
2025-03-06 19:36:11 +1100

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -7,9 +7,9 @@ shared.cfgkeys = cfgkeys
 
 cfgkeys.new = function()
     local user = rbxanalytics:GetClientId()
-    local identifier = 'CFGKEY-'..tostring(math.random(1,9999999))
+    local identifier = tostring(math.random(1,9999999))
     local key = sha.sha256(identifier)
-    cfgkeys.clientkeys[user] = key
+    cfgkeys.clientkeys[user] = 'CFGKEY-'..key
     writefile('cfgkey.lua', cfgkeys.clientkeys[user])
     return key
 end
